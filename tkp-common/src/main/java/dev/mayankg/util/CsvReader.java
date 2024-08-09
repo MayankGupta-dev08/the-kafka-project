3bb0ae6685337425f09915ed2f866f2ac7fb8b90
Mayank Gupta <ce.mayank8@gmail.com>
2024-08-09 20:19:07 +0530

    #fixed the maven build issue

--- tkp-common/src/main/java/dev/mayankg/util/CsvReader.java
+++ tkp-common/src/main/java/dev/mayankg/util/CsvReader.java
@@ -8,21 +8,26 @@ import org.springframework.core.io.ClassPathResource;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Collections;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class CsvReader {
 
+    private static final Logger LOGGER = Logger.getLogger(CsvReader.class.getName());
+
     public static List<Customer> readDataFromCsv() {
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader
-                (new ClassPathResource("users.csv").getInputStream()))) {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(
+                new ClassPathResource("users.csv").getInputStream()))) {
             CsvToBean<Customer> csvToBean = new CsvToBeanBuilder<Customer>(reader)
                     .withType(Customer.class)
                     .build();
             return csvToBean.parse();
         } catch (IOException e) {
-            e.printStackTrace();
-            // Handle the exception as needed
-            return null;
+            LOGGER.log(Level.SEVERE, "Error reading CSV file", e);
+            // Return an empty list or throw a custom exception if appropriate
+            return Collections.emptyList();
         }
     }
 }
